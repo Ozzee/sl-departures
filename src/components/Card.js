8212938bfbe4e205a9a619bbdd984fc6c96288a0
Oskar Ehnstrom <oskar.ehnstrom@futurice.com>
2017-02-05 14:04:41 +0100

    Fix some indentations

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import Departures from '../containers/departures';
  
 class Card extends Component {
-    render() {
-        return (
+  render() {
+    return (
             <div className="card">
                 <div className="card-content">
                     <span className="card-title">Fruängen</span>
                     <Departures />
                 </div>
             </div>
-        );
-    }
+    );
+  }
 }
 
 export default Card;
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import Card from './Card';
 
 class Cards extends Component {
-    render() {
-        return (<div className="row">
+  render() {
+    return (<div className="row">
             <div className="col s12 m6">
               <Card />
             </div>
       </div>);
-    }
+  }
 }
 
 export default Cards;
