e05ebaff04b9dfc97900643f1bba153960d50783
Oskar Ehnstrom <oskar.ehnstrom@futurice.com>
2017-02-06 19:44:31 +0100

    Update server

--- server.js
+++ server.js
@@ -1,33 +1,45 @@
-'use strict';
+'use strict'
+
+/*eslint-env node*/
 
 const Hapi = require('hapi')
 const fetch = require('isomorphic-fetch')
+const logger = require('bucker').createLogger()
 
-const apiKey = process.env.SL_KEY;
+
+const apiKey = process.env.SL_KEY
 
 const server = new Hapi.Server()
 server.connection({ port: 3000, host: 'localhost' })
 
 server.route({
-    method: 'GET',
-    path: '/stop/{stopId}',
-    handler: function (request, reply) {
-      fetch('http://api.sl.se/api2/realtimedeparturesV4.json?key='+apiKey+'&timewindow=30&siteid=' + request.params.stopId)
+  method: 'GET',
+  path: '/stop/{stopId}',
+  handler: function (request, reply) {
+    logger.info('Requesting stop: ' + request.params.stopId)
+    fetch('http://api.sl.se/api2/realtimedeparturesV4.json?key='+apiKey+'&timewindow=60&siteid=' + request.params.stopId)
         .then(response => response.json())
         .then(json => {
-          const data = {departures: json.ResponseData.Metros.map((stop) => {
-            return {line: stop.LineNumber, destination: stop.Destination, time: stop.DisplayTime}
-          })}
-          return reply(data).header('Access-Control-Allow-Origin', 'http://localhost:9000')
-        });
-    }
+          if (json.StatusCode !== 0) {
+            logger.error(json.Message)
+          } else {
+            logger.debug(json.ResponseData)
+            const data = {departures: json.ResponseData.Metros.map((stop) => {
+              return {line: stop.LineNumber, destination: stop.Destination, time: stop.DisplayTime}
+            })}
+            
+            return reply(data).header('Access-Control-Allow-Origin', 'http://localhost:9000')
+          }
+        })
+        .catch((error) => logger.error(error))
+  }
 })
 
 server.start((err) => {
 
-    if (err) {
-        throw err;
-    }
-    console.log(`Server running at: ${server.info.uri}`);
-});
+  if (err) {
+    throw err
+  }
+  logger.info(`Server running at: ${server.info.uri}`)
+})
 
