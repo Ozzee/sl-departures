0178ee1c94589270d78af013cf7ad493de04b637
Oskar Ehnstrom <oskar.ehnstrom@futurice.com>
2017-02-18 18:56:56 +0100

    Add direction information to cards

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,15 +2,38 @@ import { combineReducers } from 'redux'
 import { RECEIVE_STOP_DATA } from '../actions'
 
 const cardsReducer = () => {
-  return {cards: [{name: 'Fruängen', stopId: '9260', updating: false, error: false},{name: 'Slussen', stopId: '9192', updating: false, error: false}]}
+  return {cards: [
+    {
+      name: 'Fruängen',
+      stopId: '9260',
+      updating: false,
+      error: false,
+      lines: [{
+        line: '14',
+        direction: 1
+      }]
+    },
+    {
+      name: 'Slussen',
+      stopId: '9192',
+      updating: false,
+      error: false,
+      lines: [{
+        line: '14',
+        direction: 2
+      }]
+    }]}
 }
 
+/*
+ * Filter new stop information and add them to the new state
+ */
 const stopsReducer = (state, action) => {
   switch (action.type) {
   case RECEIVE_STOP_DATA:
-    var s = Object.assign({}, state)
-    s.stops[action.stop] = action.data.departures
-    return s
+    var newState = Object.assign({}, state)
+    newState.stops[action.stop] = action.data.departures
+    return newState
     
   default:
     return state == undefined ? {stops: {}} : state
